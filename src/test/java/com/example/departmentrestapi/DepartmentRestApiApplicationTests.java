0c94027f673d39a50638144fb1e6cabd924742a1
Obeta Ifeanyichukwu Malachy <angeloobeta@hotmail.com>
2023-03-17 17:46:13 +0100

    Initial commit

---
+++ src/test/java/com/example/departmentrestapi/DepartmentRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.departmentrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
